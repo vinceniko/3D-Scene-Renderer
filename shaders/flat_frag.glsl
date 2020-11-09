369971af473c180961a529f9450e4eef9dd4eac4
Vincent Nikolayev <vincent@Vincents-MacBook-Pro.local>
2020-11-09 02:12:14 -0500

    flat shading wip

---
+++ shaders/flat_frag.glsl
@@ -0,0 +1,46 @@
+#version 330 core
+in vec3 frag_pos;
+in vec3 normal;
+
+uniform vec3 triangle_color;
+
+uniform mat4 model_trans;
+uniform mat4 view_trans;
+
+out vec4 out_color;
+
+const vec3 light_pos = vec3(0.0, 20.0, 20.0);
+const vec3 light_color = vec3(0.5);
+
+const float ambient_strength = 0.5;
+
+const float specular_strength = 1.0;
+
+const float shininess = 128;
+
+void main()
+{
+    vec3 ambient = ambient_strength * light_color;
+
+    vec3 dpdx = dFdx(frag_pos);
+	vec3 dpdy = dFdy(frag_pos);
+    vec3 norm = normalize(cross(dpdx, dpdy));
+
+    vec3 light_dir = normalize(light_pos - frag_pos);  
+    float diff = max(dot(norm, light_dir), 0.0);
+    vec3 diffuse = diff * light_color;
+
+    vec3 view_dir = normalize(vec3(inverse(view_trans)[3]) - frag_pos);
+
+    // // phong
+    // vec3 reflect_dir = reflect(-light_dir, norm);
+    // float spec = pow(max(dot(view_dir, reflect_dir), 0.0), shininess);
+    
+    // blinn-phong
+    vec3 halfDir = normalize(light_dir + view_dir);
+    float spec = pow(max(dot(halfDir, norm), 0.0), shininess);
+    vec3 specular = specular_strength * spec * light_color;
+
+    vec3 result = (ambient + diffuse + specular) * triangle_color;
+    out_color = vec4(result, 1.0);
+}
