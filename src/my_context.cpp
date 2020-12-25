cae0e36c29d4faf8368551b784e5051db20766b8
Vincent Nikolayev <vincent.nikolayev@gmail.com>
2020-12-24 20:20:45 -0500

    minor refactor

--- src/my_context.cpp
+++ src/my_context.cpp
@@ -3,30 +3,34 @@
 #include "cubemap.h"
 
 MyContext::MyContext(int width, int height) :
-// the issue with creating the env->camera first is, due to inheritance, Context is initialized firstpo
-Context{
-    std::make_unique<Environment>(
-        std::make_unique<TrackballCamera>(static_cast<float>(width) / height ),
-        width, 
-        height,
-        PointLights{ PointLight{ glm::vec3(1.5f, 1.f, 0.f)} },
-        std::make_unique<GL_CubeMapEntity>(
-            "../data/night_env/",
-            true
+    // the issue with creating the env->camera first is, due to inheritance, Context is initialized firstpo
+    Context{
+        std::make_unique<Environment>(
+            std::make_unique<TrackballCamera>(static_cast<float>(width) / height),
+            width,
+            height,
+            PointLights{ PointLight{ glm::vec3(1.5f, 1.f, 0.f)} },
+            std::make_unique<GL_CubeMapEntity>(
+                "../data/night_env/",
+                true
+            )
         )
-    )
 },
 cameras{
     env->camera.get_camera_ptr(),
     new FreeCamera{ static_cast<float>(width) / height }
 } {
-    init_mesh_prototypes({ BumpyCubeMesh{}, BunnyMesh{}, MonkeyMesh{} }); 
-    
+    init_mesh_prototypes({ BumpyCubeMesh{}, BunnyMesh{}, MonkeyMesh{} });
+
     push_mesh_entity({ DefMeshList::QUAD });
     mesh_list[0].set_trans(glm::translate(glm::mat4{ 1.f }, glm::vec3(0.f, -1.f, 0.f)));
     mesh_list[0].rotate(glm::mat4{ 1.f }, -90.f, glm::vec3(1.f, 0.f, 0.f));
     mesh_list[0].scale(glm::mat4{ 1.f }, Spatial::ScaleDir::In, 10.f);
     mesh_list[0].set_color(glm::vec3(252, 137, 42) / 256.f);
+}
+
+void MyContext::switch_cube_map() {
+    env->swap_cube_map(curr_cube_map);
 }
 
 void MyContext::switch_shader() {
@@ -36,6 +40,7 @@ void MyContext::switch_shader() {
         mesh_entity.set_shader(shaders.cycle());
     }
 }
+
 void MyContext::switch_draw_mode() {
     Optional<MeshEntity> opt_mesh_entity = get_selected();
     if (opt_mesh_entity.has_value()) {
@@ -47,6 +52,7 @@ void MyContext::switch_draw_mode() {
 void MyContext::set_camera(Camera* new_camera) {
     env->camera.set_camera(std::move(std::unique_ptr<Camera>(new_camera)));
 }
+
 void MyContext::switch_camera() {
     cameras[camera_idx] = env->camera.get_camera_move().release();  // assign back to array
     camera_idx = (camera_idx + 1) % cameras.size();
--- src/my_context.h
+++ src/my_context.h
@@ -7,8 +7,8 @@
 #include "context.h"
 
 class MyContext : public Context {
-    ShaderUseList shaders = { ShaderPrograms::PHONG, ShaderPrograms::FLAT, ShaderPrograms::REFLECT, ShaderPrograms::REFRACT };
-    DrawModeUseList draw_modes = { DrawMode::DEF_DRAW_MODE, DrawMode::WIREFRAME, DrawMode::WIREFRAME_ONLY, DrawMode::DRAW_NORMALS  };
+    ShaderCycler shaders = { ShaderPrograms::PHONG, ShaderPrograms::FLAT, ShaderPrograms::REFLECT, ShaderPrograms::REFRACT };
+    DrawModeCycler draw_modes = { DrawMode::DEF_DRAW_MODE, DrawMode::WIREFRAME, DrawMode::WIREFRAME_ONLY, DrawMode::DRAW_NORMALS  };
 
     std::array<Camera*, 2> cameras;
     size_t camera_idx = 0;
@@ -25,9 +25,7 @@ public:
     // cycles through the available shader programs enumerated in ProgramList
     void switch_shader();
     void switch_draw_mode();
-    void switch_cube_map() {
-        env->swap_cube_map(curr_cube_map);
-    }
+    void switch_cube_map();
     void set_camera(Camera* new_camera);
     void switch_camera();
 
